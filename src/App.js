7c30a8a088e509a419bd166b2798034944590582
Mattia <mattiapitossi@icloud.com>
2023-01-04 20:26:42 +0100

    Add Whatsapp chat

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // src/App.js
 
-import './App.css';
+import "./App.css";
 
-import React from 'react';
-import About from './components/About';
-import Contact from './components/Contact';
-import Navbar from './components/Navbar';
-import LinkedIn from './components/LinkedIn';
-import Goals from './components/Goals';
-import Calendar from './components/Calendar';
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import LinkedIn from "./components/LinkedIn";
+import Goals from "./components/Goals";
+import Calendar from "./components/Calendar";
 
 export default function App() {
   return (
@@ -17,8 +17,16 @@ export default function App() {
       <About />
       <Goals />
       <LinkedIn />
-      <Calendar />
+      {/* <Calendar /> */}
       <Contact />
+      <a
+        href="https://wa.me/3465727999"
+        class="whatsapp_float"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fa fa-whatsapp whatsapp-icon"></i>
+      </a>
     </main>
   );
 }
