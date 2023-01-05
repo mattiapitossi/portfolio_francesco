cddf8b3c45e103622109f2d59dc485fd9ee3fd3d
Mattia <mattiapitossi@icloud.com>
2023-01-05 13:00:05 +0100

    Add form email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -68,9 +68,9 @@ export default function Contact() {
                 </div>
 
                 <form
-                    netlify
                     name="contact"
-                    onSubmit={handleSubmit}
+                    action="https://formsubmit.co/francesco.magagna@pfafineco.it"
+                    method="POST"
                     className="text-gray-500 lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <h2 className="text-gray-600 sm:text-4xl text-3xl mb-1 font-medium title-font">
                         Contattami
@@ -88,6 +88,7 @@ export default function Contact() {
                             name="name"
                             onChange={(e) => setName(e.target.value)}
                             className="bg-gray-600 bg-opacity-40 w-full rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            required
                         />
                     </div>
                     <div className="relative mb-4">
@@ -100,6 +101,7 @@ export default function Contact() {
                             name="surname"
                             onChange={(e) => setName(e.target.value)}
                             className="bg-gray-600 bg-opacity-40 w-full rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            required
                         />
                     </div>
                     <div className="relative mb-4">
@@ -124,6 +126,7 @@ export default function Contact() {
                             name="email"
                             onChange={(e) => setEmail(e.target.value)}
                             className="bg-gray-600 bg-opacity-40 w-full rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            required
                         />
                     </div>
                     <div className="relative mb-4">
@@ -135,12 +138,13 @@ export default function Contact() {
                             name="message"
                             onChange={(e) => setMessage(e.target.value)}
                             className="bg-gray-600 bg-opacity-40 w-full rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                            required
                         />
                     </div>
                     <button
                         type="submit"
                         className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                        Invia!
+                        Invia
                     </button>
                 </form>
             </div>
