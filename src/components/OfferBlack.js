03eb07095c689f79d994b8ee82b690905588d84e
Mattia <mattiapitossi@icloud.com>
2023-01-07 18:38:30 +0100

    Improve offering boxes

--- src/components/OfferBlack.js
+++ src/components/OfferBlack.js
@@ -5,19 +5,23 @@ export default function OfferBlack() {
     <section id="consultant" className="bg-gray-100 py-12">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold text-center mb-10">
-        <BriefcaseIcon className="w-10 inline-block mb-4 text-gray-500" />
-                    <h1 className="text-gray-600 title-font font-medium text-3xl sm:text-4xl mb-4">
-                    Come funziona la mia consulenza? Scegli il piano che fa più per te!
-                    </h1>
-    
+          <BriefcaseIcon className="w-10 inline-block mb-4 text-gray-500" />
+          <h1 className="text-gray-600 title-font font-medium text-3xl sm:text-4xl mb-4">
+            Come funziona la mia consulenza? Scegli il piano che fa più per te!
+          </h1>
         </h2>
         <div className="flex flex-wrap -mx-4">
           <div className="w-full md:w-1/3 px-4 mb-8">
             <div className="bg-gray-800 shadow-lg rounded-lg p-6">
-              <h3 className="text-2xl font-bold text-center mb-6 text-white">
-                Basic
-              </h3>
-              <p className="text-center mb-6 text-white">Pensato per:</p>
+              <div
+                className="bg-gray-800 shadow-lg rounded-lg p-6 half-color-before mb-6"
+                style={{ backgroundColor: "#6D7888" }}
+              >
+                <h3 className="text-2xl font-bold text-center mb-2 mt-1 text-white">
+                  Basic
+                </h3>
+              </div>
+              <p className="text-center mb-6 text-white font-bold">Pensato per:</p>
               <ul className="list-inside list-disc text-center text-white">
                 <li>Chi ha un conto Fineco e non ha un consulente abbinato</li>
                 <li>
@@ -26,7 +30,7 @@ export default function OfferBlack() {
                 </li>
               </ul>
               <div className="text-center mt-6">
-                <p className="text-center mb-3 text-white">Servizi offerti:</p>
+                <p className="text-center mb-3 text-white font-bold">Servizi offerti:</p>
                 <ul className="list-inside list-disc text-center text-white">
                   <li>Consulenza di base.</li>
                   <li>Assistenza via e-mail e Whatsapp.</li>
@@ -36,10 +40,15 @@ export default function OfferBlack() {
           </div>
           <div className="w-full md:w-1/3 px-4 mb-8">
             <div className="bg-gray-800 shadow-lg rounded-lg p-6">
-              <h3 className="text-2xl font-bold text-center mb-6 text-white">
+            <div
+                className="bg-gray-800 shadow-lg rounded-lg p-6 half-color-before mb-6"
+                style={{ backgroundColor: "#BD861F" }}
+              >
+              <h3 className="text-2xl font-bold text-center mb-2 mt-1 text-white">
                 Premium
               </h3>
-              <p className="text-center mb-6 text-white">Pensato per:</p>
+              </div>
+              <p className="text-center mb-6 text-white font-bold">Pensato per:</p>
               <ul className="list-inside list-disc text-center text-white">
                 <li>
                   Chi vuole iniziare un percorso d'investimento con un capitale
@@ -48,7 +57,7 @@ export default function OfferBlack() {
                 <li>(NB: per gli under30 il conto è sempre a canone zero)</li>
               </ul>
               <div className="text-center mt-6">
-                <p className="text-center mb-3 text-white">Servizi offerti:</p>
+                <p className="text-center mb-3 text-white font-bold">Servizi offerti:</p>
                 <ul className="list-inside list-disc text-center text-white">
                   <li>
                     Consulenza nella costruzione di un portafoglio
@@ -65,10 +74,15 @@ export default function OfferBlack() {
           </div>
           <div className="w-full md:w-1/3 px-4 mb-8">
             <div className="bg-gray-800 shadow-lg rounded-lg p-6">
-              <h3 className="text-2xl font-bold text-center mb-6 text-white">
+            <div
+                className="bg-gray-800 shadow-lg rounded-lg p-6 half-color-before mb-6"
+                style={{ backgroundColor: "#23405C" }}
+              >
+              <h3 className="text-2xl font-bold text-center mb-2 mt-1 text-white">
                 Private
               </h3>
-              <p className="text-center mb-3 text-white">Pensato per:</p>
+              </div>
+              <p className="text-center mb-3 text-white font-bold">Pensato per:</p>
               <ul className="list-inside list-disc text-center text-white">
                 <li>
                   Chi ha un portafoglio d'investimento con un capitale minimo di
@@ -77,7 +91,7 @@ export default function OfferBlack() {
                 <li>(NB: per gli under30 il conto è sempre a canone zero)</li>
               </ul>
               <div className="text-center mt-6">
-                <p className="text-center mb-3 text-white">Servizi offerti:</p>
+                <p className="text-center mb-3 text-white font-bold">Servizi offerti:</p>
                 <ul className="list-inside list-disc text-center text-white">
                   <li>Tutti i servizi del piano Premium.</li>
                   <li>Monitoraggio attivo del portafoglio.</li>
@@ -116,7 +130,6 @@ export default function OfferBlack() {
           </div>
         </div>
       </div> */}
-
     </section>
   );
 }
