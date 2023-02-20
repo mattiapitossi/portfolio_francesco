96546d5fea51c76ff46d6c5b03d5993179259eb9
Mattia <mattiapitossi@icloud.com>
2023-02-20 14:21:34 +0100

    Update file form

--- src/components/TallyEmbed.js
+++ src/components/TallyEmbed.js
@@ -204,8 +204,7 @@ export default function TallyEmbed() {
                       <div>
                         <label className="block text-sm font-medium text-gray-700">
                           Carica qui un file con la panoramica del tuo
-                          portafoglio d'investimento (qualsiasi formato è
-                          ammesso: Pdf / Excel / Word / Jpeg...)
+                          portafoglio d'investimento
                         </label>
                         <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
                           <div className="space-y-1 text-center">
@@ -228,7 +227,7 @@ export default function TallyEmbed() {
                                 htmlFor="file-upload"
                                 className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
                               >
-                                <span>Upload a file</span>
+                                <span>Carica un file</span>
                                 <input
                                   id="file-upload"
                                   name="file-upload"
@@ -236,10 +235,10 @@ export default function TallyEmbed() {
                                   className="sr-only"
                                 />
                               </label>
-                              <p className="pl-1">or drag and drop</p>
+                              <p className="pl-1">oppure drag and drop</p>
                             </div>
                             <p className="text-xs text-gray-500">
-                              PNG, JPG, GIF up to 10MB
+                              PNG, JPG, GIF fino a 10MB
                             </p>
                           </div>
                         </div>
@@ -250,7 +249,7 @@ export default function TallyEmbed() {
                         type="submit"
                         className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                       >
-                        Save
+                        Invia
                       </button>
                     </div>
                   </div>
