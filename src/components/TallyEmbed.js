aa6b82d4f6fe93c21ce01e9510d839096f8abb3e
Mattia <mattiapitossi@icloud.com>
2023-02-20 19:59:35 +0100

    Add undo upload file

--- src/components/TallyEmbed.js
+++ src/components/TallyEmbed.js
@@ -32,6 +32,10 @@ export default function TallyEmbed() {
   const handleDragOver = (event) => {
     event.preventDefault();
   };
+
+  function handleUndoClick() {
+    setFile(null);
+  }
   return (
     <section id="analysis" className="bg-gray-100 py-12">
       <h2 className="text-3xl font-bold text-center mb-10">
@@ -225,47 +229,50 @@ export default function TallyEmbed() {
                           className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6"
                           onDrop={handleDrop}
                           onDragOver={handleDragOver}
-                        > {file ? (
+                        >
+                          {" "}
+                          {file ? (
                             <div>
-                              <p>File caricato: {file.name}</p>
+                              <p className="text-gray-900">File caricato: {file.name}</p>
+                              <button  className="text-red-900" onClick={handleUndoClick}>Annulla</button>
                             </div>
                           ) : (
-                          <div className="space-y-1 text-center">
-                            <svg
-                              className="mx-auto h-12 w-12 text-gray-400"
-                              stroke="currentColor"
-                              fill="none"
-                              viewBox="0 0 48 48"
-                              aria-hidden="true"
-                            >
-                              <path
-                                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                                strokeWidth={2}
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              />
-                            </svg>
-                            <div className="flex text-sm text-gray-600">
-                              <label
-                                htmlFor="file-upload"
-                                className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
+                            <div className="space-y-1 text-center">
+                              <svg
+                                className="mx-auto h-12 w-12 text-gray-400"
+                                stroke="currentColor"
+                                fill="none"
+                                viewBox="0 0 48 48"
+                                aria-hidden="true"
                               >
-                                <span>Carica un file</span>
-                                <input
-                                  id="file-upload"
-                                  name="file-upload"
-                                  type="file"
-                                  className="sr-only"
-                                  accept="image/png, image/jpeg, application/pdf"
-                                  onChange={handleFileChange}
+                                <path
+                                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                                  strokeWidth={2}
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
                                 />
-                              </label>
-                              <p className="pl-1">oppure drag and drop</p>
+                              </svg>
+                              <div className="flex text-sm text-gray-600">
+                                <label
+                                  htmlFor="file-upload"
+                                  className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
+                                >
+                                  <span>Carica un file</span>
+                                  <input
+                                    id="file-upload"
+                                    name="file-upload"
+                                    type="file"
+                                    className="sr-only"
+                                    accept="image/png, image/jpeg, application/pdf"
+                                    onChange={handleFileChange}
+                                  />
+                                </label>
+                                <p className="pl-1">oppure drag and drop</p>
+                              </div>
+                              <p className="text-xs text-gray-500">
+                                PNG, JPG, PDF fino a 5MB
+                              </p>
                             </div>
-                            <p className="text-xs text-gray-500">
-                              PNG, JPG, PDF fino a 5MB
-                            </p>
-                          </div>
                           )}
                         </div>
                       </div>
